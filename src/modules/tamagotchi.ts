5730032a350b062b2ab6bd182a2d61e3791244ba
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2023-12-02 01:05:13 +0100

    cr issues

--- src/modules/tamagotchi.ts
+++ src/modules/tamagotchi.ts
@@ -15,7 +15,7 @@ export default class Tamagotchi extends Abilities {
   constructor() {
     super();
     this.health = { value: 10, importance: 1 };
-    this.hunger = { value: 5, importance: 3 };
+    this.hunger = { value: 10, importance: 3 };
     this.energy = { value: 10, importance: 2 };
     this.fun = { value: 10, importance: 4 };
     console.log("Tamagotchi initialized");
